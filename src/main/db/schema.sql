5e06db11d80584082cfa38c962cd28f06b02c5ad
Baptcarf <baptiste.carfantan72@gmail.com>
2026-02-04 14:24:28 +0100

    ajout du schéma de la db

---
+++ src/main/db/schema.sql
@@ -0,0 +1,25 @@
+
+-- Table des informations de l'utilisateur
+CREATE TABLE Utilisateur(
+        id_utilisateur SERIAL PRIMARY KEY,
+        pseudo VARCHAR(50) NOT NULL, 
+        Couleur VARCHAR(7) NOT NULL -- La couleur du compte
+);
+
+-- Table des grilles présent dans le jeu
+CREATE TABLE Grille(
+        id_grille SERIAL PRIMARY KEY,
+        niveau INT NOT NULL, -- Le niveau de difficulté de la grille
+        grille_dep BLOB NOT NULL -- la grille de départ
+);
+
+
+-- Table des parties 
+CREATE TABLE Partie(
+        id_utilisateur BIGINT REFERENCES Utilisateur(id_utilisateur),
+        id_grille BIGINT REFERENCES Grille(id_grille),
+        statut INT NOT NULL, -- Le statut de la partie (0 pas commencé, 1 en cours, 2 terminé)
+        score INT , -- Le score de l'utilisateur si la partie est terminé
+        sauvegarde BLOB, -- le fichier contenant la sauvegarde de la partie du joueur
+        PRIMARY KEY (id_utilisateur, id_grille)
+);
