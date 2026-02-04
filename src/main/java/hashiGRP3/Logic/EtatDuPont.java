f8dfeaf9114180c98fecb92128bf3824c9aebf79
Barth <derouaultbarthelemy@protonmail.com>
2026-02-04 13:41:48 +0100

    Commentaire de EtatDuPont

--- src/main/java/hashiGRP3/Logic/EtatDuPont.java
+++ src/main/java/hashiGRP3/Logic/EtatDuPont.java
@@ -1,17 +1,34 @@
 package hashiGRP3.Logic;
 
+/**
+ * Represente l'etat d'un pont entre deux iles.
+ * Un pont peut etre vide, simple ou double.
+ * Chaque etat possede une valeur numerique.
+ */
 public enum EtatDuPont {
 
+    /** Aucun pont present */
     VIDE(0),
+    /** Pont simple */
     SIMPLE(1),
+    /** Pont double */
     DOUBLE(2);
 
+    /** Valeur du pont */
     private final int value;
 
+    /**
+     * Construit un etat de pont avec sa valeur associee
+     * @param value valeur de l'etat
+     */
     EtatDuPont(int value) {
         this.value = value;
     }
 
+    /**
+     * Retourne la valeur numerique de l'etat du pont
+     * @return valeur de l'etat (0, 1 ou 2)
+     */
     public int getValue() {
         return value;
     }
