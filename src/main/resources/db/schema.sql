c774f8dfb90c1cc2db69d2e631c25ba015947e97
Baptcarf <baptiste.carfantan72@gmail.com>
2026-02-05 16:08:09 +0100

    modification db

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -17,19 +17,28 @@ CREATE TABLE Grille(
 -- Table des parties 
 CREATE TABLE Partie(
         id_partie INTEGER PRIMARY KEY AUTOINCREMENT,
-        id_utilisateur BIGINT REFERENCES Utilisateur(id_utilisateur),
-        id_grille BIGINT REFERENCES Grille(id_grille),
+        id_utilisateur BIGINT,
+        id_grille BIGINT,
         statut INT NOT NULL, -- Le statut de la partie (0 pas commencé, 1 en cours, 2 terminé)
         score INT , -- Le score de l'utilisateur si la partie est terminé
-        sauvegarde BLOB -- le fichier contenant la sauvegarde de la partie du joueur
+        sauvegarde BLOB, -- le fichier contenant la sauvegarde de la partie du joueur
+        FOREIGN KEY (id_utilisateur)
+            REFERENCES Utilisateur(id_utilisateur)
+            ON DELETE CASCADE,
+        FOREIGN KEY (id_grille)
+            REFERENCES Grille(id_grille)
+            ON DELETE CASCADE
 );
 
 
 CREATE TABLE Coup(
-        id_partie BIGINT REFERENCES Partie(id_partie),
+        id_partie BIGINT,
         num_coup BIGINT, -- Le numéro du coûp
         node_dep VARCHAR(20), -- le noeud de départ de l'arréte
         node_arr VARCHAR(20), -- le noeud d'arrivé de l'arréte
         val_coup INTEGER, -- la valeur de l'arréte (0 : vide, 1 : 1 arrêtes, 2 : 2 arrêtes)
-        PRIMARY KEY(id_partie,num_coup)
+        PRIMARY KEY(id_partie,num_coup),
+        FOREIGN KEY (id_partie)
+            REFERENCES Partie(id_partie)
+            ON DELETE CASCADE
 );
