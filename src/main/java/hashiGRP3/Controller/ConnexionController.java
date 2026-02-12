9cf7b5cc8232b8fc2d3c1499f92afd9968ba836c
Baptcarf <baptiste.carfantan72@gmail.com>
2026-02-12 14:30:58 +0100

    vérification du nom d'utilisateur avant la création du compte

--- src/main/java/hashiGRP3/Controller/ConnexionController.java
+++ src/main/java/hashiGRP3/Controller/ConnexionController.java
@@ -29,247 +29,247 @@ import java.util.List;
 /* Class */
 public class ConnexionController extends ManageController {
 
-        private int nbCount;
+    private int nbCount;
 
-        private boolean sup = false;
-        private boolean mod = false;
-        @FXML
-        private HBox hbox;
+    private boolean sup = false;
+    private boolean mod = false;
+    @FXML
+    private HBox hbox;
 
-        @FXML
-        private Circle creer;
+    @FXML
+    private Circle creer;
 
-        @FXML
-        private Label labelCreer;
+    @FXML
+    private Label labelCreer;
 
-        @FXML
-        private Button supprimer;
+    @FXML
+    private Button supprimer;
 
-        public void setSceneManager(SceneManager sm) {
-                super.setSceneManager(sm);
-                nbCount = 0;
-                Tooltip.install(creer, new Tooltip("Créer un compte"));
-                Tooltip.install(supprimer, new Tooltip("supprimer un compte"));
+    public void setSceneManager(SceneManager sm) {
+        super.setSceneManager(sm);
+        nbCount = 0;
+        Tooltip.install(creer, new Tooltip("Créer un compte"));
+        Tooltip.install(supprimer, new Tooltip("supprimer un compte"));
 
-                List<Utilisateur> au = sm.getBD().findAllUser();
+        List<Utilisateur> au = sm.getBD().findAllUser();
 
-                for (Utilisateur u : au) {
-                        Circle c = createCircle(u.getColor());
-                        createBoxUser(c, u.getPseudo());
-                        nbCount++;
-                }
-                if (nbCount == 5) {
-                        creer.setFill(Color.web("#DFDFDFDF"));
-                }
-
+        for (Utilisateur u : au) {
+            Circle c = createCircle(u.getColor());
+            createBoxUser(c, u.getPseudo());
+            nbCount++;
+        }
+        if (nbCount == 5) {
+            creer.setFill(Color.web("#DFDFDFDF"));
         }
 
-        @FXML
-        private void changeScene(ActionEvent event) {
-                Button btn = (Button) event.getSource();
-                String sceneName = (String) btn.getUserData();
-                if (getSceneManager() != null && sceneName != null) {
-                        getSceneManager().changeScene(sceneName);
-                }
+    }
 
+    @FXML
+    private void changeScene(ActionEvent event) {
+        Button btn = (Button) event.getSource();
+        String sceneName = (String) btn.getUserData();
+        if (getSceneManager() != null && sceneName != null) {
+            getSceneManager().changeScene(sceneName);
         }
 
-        private void createBoxUser(Circle c, String val) {
+    }
 
-                VBox v = new VBox();
-                v.setAlignment(Pos.CENTER);
-                v.setSpacing(8);
-                v.setMinHeight(150);
-                v.setPrefHeight(150);
-                Label l = new Label(val);
-                l.setAlignment(Pos.CENTER);
-                v.getChildren().addAll(c, l);
+    private void createBoxUser(Circle c, String val) {
 
-                hbox.getChildren().add(0, v);
+        VBox v = new VBox();
+        v.setAlignment(Pos.CENTER);
+        v.setSpacing(8);
+        v.setMinHeight(150);
+        v.setPrefHeight(150);
+        Label l = new Label(val);
+        l.setAlignment(Pos.CENTER);
+        v.getChildren().addAll(c, l);
 
-        }
+        hbox.getChildren().add(0, v);
 
-        /**
-         * Creer un cercle de connexion à afficher.
-         */
-        private Circle createCircle(String color) {
-                // Apparence
-                Circle circle = new Circle();
-                circle.setRadius(100); // même taille que rightCircle
-                circle.setFill(Color.web(color));
-                circle.setStroke(Color.BLACK);
-                circle.setStrokeWidth(2);
+    }
 
-                // Logique
-                circle.setOnMouseClicked(event -> {
-                        if (sup) {
+    /**
+     * Creer un cercle de connexion à afficher.
+     */
+    private Circle createCircle(String color) {
+        // Apparence
+        Circle circle = new Circle();
+        circle.setRadius(100); // même taille que rightCircle
+        circle.setFill(Color.web(color));
+        circle.setStroke(Color.BLACK);
+        circle.setStrokeWidth(2);
 
-                                VBox parentVBox = (VBox) circle.getParent();
-                                Label lab = null;
+        // Logique
+        circle.setOnMouseClicked(event -> {
+            if (sup) {
 
-                                // Parcours les enfants du VBox
-                                for (Node node : parentVBox.getChildren()) {
-                                        if (node instanceof Label) {
-                                                lab = (Label) node;
-                                                break;
-                                        }
-                                }
+                VBox parentVBox = (VBox) circle.getParent();
+                Label lab = null;
 
-                                setUtilisateur(lab.getText());
-
-                                supprimerCompte(circle);
-
-                        } else {
-                                getSceneManager().changeScene("accueil");
-                        }
-
-                });
-
-                // Retour
-                return circle;
-        }
-
-        /**
-         * Ajoute un cercle de connexion à la page d'accueil.
-         */
-        @FXML
-        private void addCount() {
-                if (nbCount < 6) {
-                        endSupp();
-
-                        Circle circle = createCircle("#eaf5ff");
-
-                        this.creerUtilisateur(circle);
-
+                // Parcours les enfants du VBox
+                for (Node node : parentVBox.getChildren()) {
+                    if (node instanceof Label) {
+                        lab = (Label) node;
+                        break;
+                    }
                 }
 
-        }
+                setUtilisateur(lab.getText());
 
-        public String colorToString(Color c) {
+                supprimerCompte(circle);
 
-                String couleurCSS = String.format("#%02X%02X%02X",
-                                (int) (c.getRed() * 255),
-                                (int) (c.getGreen() * 255),
-                                (int) (c.getBlue() * 255));
+            } else {
+                getSceneManager().changeScene("accueil");
+            }
 
-                return couleurCSS;
+        });
 
-        }
+        // Retour
+        return circle;
+    }
 
-        private void appliqueCouleur(Color c) {
-                String couleurCSS = colorToString(c);
+    /**
+     * Ajoute un cercle de connexion à la page d'accueil.
+     */
+    @FXML
+    private void addCount() {
+        if (nbCount < 6) {
+            endSupp();
 
-                String value = String.format("-fx-background-color:%s;", couleurCSS);
-                getSceneManager().findScene("connexion").getRoot()
-                                .setStyle(value);
+            Circle circle = createCircle("#eaf5ff");
 
-        }
-
-        @FXML
-        private void supCompte() {
-                appliqueCouleur(Color.web("#E57373"));
-                sup = true;
-        }
-
-        private void endSupp() {
-                appliqueCouleur(Color.WHITE);
-                sup = false;
-        }
-
-        private void supprimerCompte(Circle circle) {
-                Stage s = new Stage();
-
-                s.setTitle("Supprimer un compte");
-
-                s.setOnCloseRequest(ev -> {
-                        endSupp();
-                });
-
-                VBox v = new VBox();
-                v.setAlignment(Pos.CENTER);
-                v.setSpacing(15);
-
-                HBox h = new HBox();
-                h.setAlignment(Pos.CENTER);
-                h.setSpacing(20);
-
-                Label l = new Label("Voulez-vous vraiment supprimer ce compte ?");
-                Button bn = new Button("Annuler");
-                Button bo = new Button("Valider");
-
-                h.getChildren().addAll(bn, bo);
-
-                v.getChildren().addAll(l, h);
-
-                bn.setOnAction(ev -> {
-                        s.close();
-                        endSupp();
-                });
-
-                bo.setOnAction(ev -> {
-                        DatabaseManager db = getSceneManager().getBD();
-
-                        VBox parentVBox = (VBox) circle.getParent();
-                        Label lab = null;
-
-                        // Parcours les enfants du VBox
-                        for (Node node : parentVBox.getChildren()) {
-                                if (node instanceof Label) {
-                                        lab = (Label) node;
-                                        break;
-                                }
-                        }
-
-                        db.deleteUser(lab.getText());
-
-                        hbox.getChildren().remove(circle.getParent());
-                        nbCount--;
-                        s.close();
-                        endSupp();
-                        creer.setFill(Color.web("#eaf5ff"));
-                });
-
-                Scene sn = new Scene(v);
-                s.setScene(sn);
-                s.show();
+            this.creerUtilisateur(circle);
 
         }
 
-        @FXML
-        private void afficheMessage() {
-                if (nbCount == 5) {
-                        labelCreer.setVisible(true);
+    }
+
+    public String colorToString(Color c) {
+
+        String couleurCSS = String.format("#%02X%02X%02X",
+                (int) (c.getRed() * 255),
+                (int) (c.getGreen() * 255),
+                (int) (c.getBlue() * 255));
+
+        return couleurCSS;
+
+    }
+
+    private void appliqueCouleur(Color c) {
+        String couleurCSS = colorToString(c);
+
+        String value = String.format("-fx-background-color:%s;", couleurCSS);
+        getSceneManager().findScene("connexion").getRoot()
+                .setStyle(value);
+
+    }
+
+    @FXML
+    private void supCompte() {
+        appliqueCouleur(Color.web("#E57373"));
+        sup = true;
+    }
+
+    private void endSupp() {
+        appliqueCouleur(Color.WHITE);
+        sup = false;
+    }
+
+    private void supprimerCompte(Circle circle) {
+        Stage s = new Stage();
+
+        s.setTitle("Supprimer un compte");
+
+        s.setOnCloseRequest(ev -> {
+            endSupp();
+        });
+
+        VBox v = new VBox();
+        v.setAlignment(Pos.CENTER);
+        v.setSpacing(15);
+
+        HBox h = new HBox();
+        h.setAlignment(Pos.CENTER);
+        h.setSpacing(20);
+
+        Label l = new Label("Voulez-vous vraiment supprimer ce compte ?");
+        Button bn = new Button("Annuler");
+        Button bo = new Button("Valider");
+
+        h.getChildren().addAll(bn, bo);
+
+        v.getChildren().addAll(l, h);
+
+        bn.setOnAction(ev -> {
+            s.close();
+            endSupp();
+        });
+
+        bo.setOnAction(ev -> {
+            DatabaseManager db = getSceneManager().getBD();
+
+            VBox parentVBox = (VBox) circle.getParent();
+            Label lab = null;
+
+            // Parcours les enfants du VBox
+            for (Node node : parentVBox.getChildren()) {
+                if (node instanceof Label) {
+                    lab = (Label) node;
+                    break;
                 }
+            }
 
+            db.deleteUser(lab.getText());
+
+            hbox.getChildren().remove(circle.getParent());
+            nbCount--;
+            s.close();
+            endSupp();
+            creer.setFill(Color.web("#eaf5ff"));
+        });
+
+        Scene sn = new Scene(v);
+        s.setScene(sn);
+        s.show();
+
+    }
+
+    @FXML
+    private void afficheMessage() {
+        if (nbCount == 5) {
+            labelCreer.setVisible(true);
         }
 
-        @FXML
-        private void enleverMessage() {
-                labelCreer.setVisible(false);
+    }
 
-        }
+    @FXML
+    private void enleverMessage() {
+        labelCreer.setVisible(false);
 
-        private void creerUtilisateur(Circle c) {
-                CreerUtilisateur dialog = new CreerUtilisateur();
+    }
 
-                dialog.showAndWait(hbox.getScene().getWindow()).ifPresent(result -> {
-                        DatabaseManager db = getSceneManager().getBD();
+    private void creerUtilisateur(Circle c) {
+        CreerUtilisateur dialog = new CreerUtilisateur();
 
-                        db.insertUser(result.pseudo(), colorToString(result.color()));
-                        c.setFill(result.color());
-                        createBoxUser(c, result.pseudo());
+        dialog.showAndWait(hbox.getScene().getWindow(), getSceneManager().getBD()).ifPresent(result -> {
+            DatabaseManager db = getSceneManager().getBD();
 
-                        nbCount++;
-                        if (nbCount == 5) {
-                                creer.setFill(Color.web("#DFDFDFDF"));
-                        }
+            db.insertUser(result.pseudo(), colorToString(result.color()));
+            c.setFill(result.color());
+            createBoxUser(c, result.pseudo());
 
-                        if (result.isNewPlayer()) {
-                                getSceneManager().changeScene("selectTutoriel");
-                        } else {
-                                getSceneManager().changeScene("accueil");
-                        }
-                });
-        }
+            nbCount++;
+            if (nbCount == 5) {
+                creer.setFill(Color.web("#DFDFDFDF"));
+            }
+
+            if (result.isNewPlayer()) {
+                getSceneManager().changeScene("selectTutoriel");
+            } else {
+                getSceneManager().changeScene("accueil");
+            }
+        });
+    }
 
 }
--- src/main/java/hashiGRP3/Scene/CreerUtilisateur.java
+++ src/main/java/hashiGRP3/Scene/CreerUtilisateur.java
@@ -1,8 +1,6 @@
 //Attribut au packet
 package hashiGRP3.Scene;
 
-
-
 //Imports
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -18,69 +16,75 @@ import javafx.stage.Stage;
 import javafx.stage.Window;
 import java.util.Optional;
 
+import hashiGRP3.DatabaseManager;
 
 /* Class */
 public class CreerUtilisateur {
 
-	public record Result(String pseudo, Color color, boolean isNewPlayer) {}
+    public record Result(String pseudo, Color color, boolean isNewPlayer) {
+    }
 
-	private final TextField pseudoField = new TextField();
-	private final ColorPicker cpField = new ColorPicker(Color.WHITE);
-	private final CheckBox cbField = new CheckBox();
-	private final Label messageLabel = new Label();
+    private final TextField pseudoField = new TextField();
+    private final ColorPicker cpField = new ColorPicker(Color.WHITE);
+    private final CheckBox cbField = new CheckBox();
+    private final Label messageLabel = new Label();
 
-	public Optional<Result> showAndWait(Window parent) {
+    public Optional<Result> showAndWait(Window parent, DatabaseManager db) {
 
-		//Creer la fenetre
-		Stage stage = new Stage();
-		stage.initModality(Modality.APPLICATION_MODAL);
-		stage.initOwner(parent);
-		stage.setTitle("Créer un compte");
+        // Creer la fenetre
+        Stage stage = new Stage();
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.initOwner(parent);
+        stage.setTitle("Créer un compte");
 
-		//Init la grid
-		GridPane grid = new GridPane();
-		grid.setHgap(10);
-		grid.setVgap(10);
-		grid.setAlignment(Pos.CENTER);
+        // Init la grid
+        GridPane grid = new GridPane();
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setAlignment(Pos.CENTER);
 
-		//Composant
-		cbField.setSelected(true);
-		messageLabel.setTextFill(Color.RED);
-		Button btn = new Button("Commencer l'aventure");
+        // Composant
+        cbField.setSelected(true);
+        messageLabel.setTextFill(Color.RED);
+        Button btn = new Button("Commencer l'aventure");
 
-		//Setup la grid
-		grid.addRow(0, new Label("Pseudo:"), pseudoField);
-		grid.addRow(1, new Label("Couleur du compte:"), cpField);
-		grid.addRow(2, new Label("Je n'ai jamais joué au Hashi : "), cbField);
-		grid.add(btn, 0, 3, 2, 1);
-		grid.add(messageLabel, 0, 4, 2, 1);
+        // Setup la grid
+        grid.addRow(0, new Label("Pseudo:"), pseudoField);
+        grid.addRow(1, new Label("Couleur du compte:"), cpField);
+        grid.addRow(2, new Label("Je n'ai jamais joué au Hashi : "), cbField);
+        grid.add(btn, 0, 3, 2, 1);
+        grid.add(messageLabel, 0, 4, 2, 1);
 
-		final Result[] resultHolder = new Result[1];
+        final Result[] resultHolder = new Result[1];
 
-		//Handler
-		btn.setOnAction(ev -> {
-		    String pseudo = pseudoField.getText();
+        // Handler
+        btn.setOnAction(ev -> {
+            String pseudo = pseudoField.getText();
 
-		    if (pseudo.isEmpty()) {
-			messageLabel.setText("Erreur : Pseudo non renseigné");
-			return;
-		    }
+            if (pseudo.isEmpty()) {
+                messageLabel.setText("Erreur : Pseudo non renseigné");
+                return;
+            }
 
-		    resultHolder[0] = new Result(
-			    pseudo,
-			    cpField.getValue(),
-			    cbField.isSelected()
-		    );
+            if (db.userExist(pseudo)) {
+                messageLabel.setText("Erreur : username déjà pris");
+                return;
 
-		    stage.close();
-		});
+            }
 
-		//
-		Scene sc = new Scene(grid, 400, 400);
-		stage.setScene(sc);
-		stage.showAndWait();
+            resultHolder[0] = new Result(
+                    pseudo,
+                    cpField.getValue(),
+                    cbField.isSelected());
 
-		return Optional.ofNullable(resultHolder[0]);
-	}
+            stage.close();
+        });
+
+        //
+        Scene sc = new Scene(grid, 400, 400);
+        stage.setScene(sc);
+        stage.showAndWait();
+
+        return Optional.ofNullable(resultHolder[0]);
+    }
 }
-
